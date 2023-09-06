9bf5cea4d2f638c9929055d0c3ebb9da0a3e9422
Vidar Skogvoll <vidarsko@fys.uio.no>
2023-09-06 19:22:13 +0200

    Update 230906vs_testing_angle_plot.py

--- development/230906vs_testing_angle_plot.py
+++ development/230906vs_testing_angle_plot.py
@@ -2,6 +2,6 @@ from comfit.core.base_system import BaseSystem
 
 bec = BaseSystem(2, xRes=101, yRes=101)
 
-field = bec.calc_angle_field_double_vortex()
+field = bec.calc_angle_field_single_vortex()
 
 bec.plot_angle_field(field)
