ff0492cb751464c8fb75cf63ed55ec769b867069
Vidar Skogvoll <vidarsko@fys.uio.no>
2023-10-13 11:58:44 +0200

    Create 231013vs_implementing_defect_tracking.py

---
+++ development/231013vs_implementing_defect_tracking.py
@@ -0,0 +1,14 @@
+import comfit as cf
+
+import comfit as cf
+import matplotlib.pyplot as plt
+
+
+bec = cf.BEC(2,xRes=61,yRes=61)
+bec.set_initial_condition_disordered()
+bec.evolve_relax_BEC(200)
+
+
+bec.plot_field(abs(bec.psi))
+
+plt.show()
