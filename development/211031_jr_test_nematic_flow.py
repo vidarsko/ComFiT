6a6aa6858168e5234184d703ede98b284bb77509
jonasron <46788041+jonasron@users.noreply.github.com>
2023-11-07 08:04:11 +0900

    Update 211031_jr_test_nematic_flow.py

--- development/211031_jr_test_nematic_flow.py
+++ development/211031_jr_test_nematic_flow.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import comfit as cf
 
-nem = cf.nematic(2,xRes=32,yRes=32,dx=1,dy=1,dt=0.01)
+nem = cf.nematic(2,xRes=32,yRes=32,dx=1,dy=1,dt=0.05)
 
 nem.set_initial_condition_disordered(noise_strength=0.5)
 nem.evolve_nematic_no_flow(5)
