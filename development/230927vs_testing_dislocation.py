c9161dd0896dc035975cdebf154c5326ee47b56e
Vidar Skogvoll <vidarsko@fys.uio.no>
2023-11-13 09:18:36 +0100

    Jupyter lab
    Learning more about jupyter lab. It seems that in order for packages to be available for the files located in e.g. development, the file needs to be intiated from the parent folder before being moved to the development folder.

--- development/230927vs_testing_dislocation.py
+++ development/230927vs_testing_dislocation.py
@@ -6,7 +6,6 @@ pfc = cf.PFCtri(13,7)
 eta = pfc.conf_insert_dislocation_dipole(dislocation_type=1)
 pfc.calc_from_amplitudes(eta)
 
-#ax = plt.gcf().add_subplot(111)
 
 for n in range(10):
     plt.gcf().clear()
