ceaf31798797eedfe0c22a3396052ca441ff281f
Vidar Skogvoll <vidarsko@fys.uio.no>
2023-12-07 19:51:01 +0100

    Update test_phase_field_crystal.py

--- tests/test_phase_field_crystal.py
+++ tests/test_phase_field_crystal.py
@@ -8,19 +8,10 @@ import comfit as cf
 
 class TestPhaseFieldCrystal(unittest.TestCase):
 
-    def test_phase_field_crystal_init(self):
-        """Test PhaseFieldCrystal initialization with a dimension parameter."""
-        for dim in [1, 2, 3]:
-            try:
-                pfc = cf.PhaseFieldCrystal(dimension=dim)
-                self.assertIsInstance(pfc, cf.PhaseFieldCrystal)
-            except Exception as e:
-                self.fail(f"Initialization of PhaseFieldCrystal failed with dimension {dim}: {e}")
-
     def test_phase_field_crystal_2d_triangular_init(self):
         """Test PhaseFieldCrystal2DTriangular initialization with nx=ny=1."""
         try:
-            pfc1d = cf.PhaseFieldCrystal1DPeriodic(1, 1)
+            pfc1d = cf.PhaseFieldCrystal2DTriangular(1, 1)
             self.assertIsInstance(pfc1d, cf.PhaseFieldCrystal2DTriangular)
         except Exception as e:
             self.fail(f"Initialization of PhaseFieldCrystal1DPeriodic failed: {e}")
