11f828c779b23ef826f356feab55bae94ab2c65c
Vidar Skogvoll <vidarsko@fys.uio.no>
2023-12-07 18:43:44 +0100

    Tests that work

--- development/231207vs_demonstrating_comfit.py
+++ development/231207vs_demonstrating_comfit.py
@@ -1,5 +1,6 @@
 import comfit as cf
 import numpy as np
+import matplotlib.pyplot as plt
 
 bec = cf.BEC(2,xRes=51,yRes=51,gamma=0.05)
 bec.conf_insert_vortex_dipole()
@@ -9,6 +10,8 @@ for n in range(50):
     bec.evolve_dGPE(100)
     bec.plot_field(np.abs(bec.psi),colormap='winter',cmap_symmetric=False,
                    clims=[0,1])
-    cf.tool_save_plot(n)
+    plt.draw()
+    plt.pause(0.01)
+    #cf.tool_save_plot(n)
 
-cf.tool_make_animation(n)
+# cf.tool_make_animation(n)
--- tests/test_base_system.py
+++ tests/test_base_system.py
@@ -11,27 +11,27 @@ class TestBaseSystem(unittest.TestCase):
 
     def test_init_empty(self):
         """ Test BaseSystem initialization with  parameters """
-        for dim in range(3):
+        for dim in [1,2,3]:
             try:
                 bs = cf.BaseSystem(dim)
                 self.assertIsInstance(bs, cf.BaseSystem)
             except Exception as e:
                 self.fail(f"Initialization failed with no parameters: {e}")
 
-    # def test_init_with_params(self):
-    #     """ Test BaseSystem initialization with different parameters """
-    #     params = [
-    #         {'param1': 'value1', 'param2': 10},
-    #         {'param1': 'value2', 'param2': 20},
-    #         # Add more parameter combinations as needed
-    #     ]
-    #     for p in params:
-    #         with self.subTest(p=p):
-    #             try:
-    #                 bs = cf.BaseSystem(**p)
-    #                 self.assertIsInstance(bs, cf.BaseSystem)
-    #             except Exception as e:
-    #                 self.fail(f"Initialization failed with parameters {p}: {e}")
+    def test_init_with_dim_1_and_params(self):
+        """ Test BaseSystem initialization with different parameters """
+        params = [
+            {'xRes': 11, 'dx': 0.1},
+            {'xRes': 32, 'dx': 0.3},
+            # Add more parameter combinations as needed
+        ]
+        for p in params:
+            with self.subTest(p=p):
+                try:
+                    bs = cf.BaseSystem(1,**p)
+                    self.assertIsInstance(bs, cf.BaseSystem)
+                except Exception as e:
+                    self.fail(f"Initialization failed with parameters {p}: {e}")
 
 if __name__ == '__main__':
     unittest.main()
