bdb05b28af5f58f7b184d6f94b19d09154dff8f8
Vidar Skogvoll <vidarsko@fys.uio.no>
2023-12-21 00:39:34 +0100

    Nothing big

--- development/231220vs_testing_snippet.py
+++ development/231220vs_testing_snippet.py
@@ -2,4 +2,11 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import comfit as cf
+import matplotlib.pyplot as plt
 
+pfc = cf.BoseEinsteinCondensate(3)
+pfc.conf_insert_vortex_ring() 
+pfc.evolve_relax(100)
+pfc.plot_field(abs(pfc.psi))
+
+plt.show()
