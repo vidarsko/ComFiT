0088f96860c855017b07628791d44e6b2b456056
jonasron <46788041+jonasron@users.noreply.github.com>
2023-12-25 13:40:40 +0900

    testing making a test

--- tests/test_nematic_liquid_crystal.py
+++ tests/test_nematic_liquid_crystal.py
@@ -5,6 +5,8 @@ import os
 # Adjust the path to import the comfit package
 sys.path.append(os.path.abspath('../'))
 import comfit as cf
+import numpy as np
+import matplotlib.pyplot as plt
 
 class TestNematicLiquidCrystal(unittest.TestCase):
 
@@ -16,6 +18,21 @@ class TestNematicLiquidCrystal(unittest.TestCase):
                 self.assertIsInstance(nlc, cf.NematicLiquidCrystal)
             except Exception as e:
                 self.fail(f"Initialization failed with dimension {dim}: {e}")
+
+    def test_no_flow_evolver(self):
+        """Test the enm.evolve_nematic_no_flow"""
+        nem = cf.NematicLiquidCrystal(2, xRes=13, yRes=4)
+        nem.conf_initial_condition_disordered()
+        nem.evolve_nematic_no_flow(300)
+
+        # Set the tolerance for approximation
+        tolerance = 0.01
+
+        # Check if all elements in bec.psi are approximately 1
+        condition = np.allclose(abs(nem.Q[0][0]), 1, atol=tolerance)
+        condition = np.allclose(abs(nem.Q[0][1]), 0, atol=tolerance)
+        self.assertTrue(condition, "Elements in nem.Q are not approximately 1")
+
 
 if __name__ == '__main__':
     unittest.main()
