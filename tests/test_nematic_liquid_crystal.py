919bb8ef186ba75d1b3c6a9442469b69ba050962
jonasron <46788041+jonasron@users.noreply.github.com>
2023-12-27 13:29:36 +0900

    test of dipole tracking nematic

--- tests/test_nematic_liquid_crystal.py
+++ tests/test_nematic_liquid_crystal.py
@@ -36,17 +36,34 @@ class TestNematicLiquidCrystal(unittest.TestCase):
 
     def test_nematic_evolver_with_defect_dipole(self):
         """Test the enm.evolve_nematic_no_flow"""
-        nem = cf.NematicLiquidCrystal(2, xRes=21, yRes=14)
-        nem.conf_initial_condition_disordered()
-        nem.evolve_nematic_no_flow(300)
+        nem = cf.NematicLiquidCrystal(2, xRes=31, yRes=31)
+        nem.conf_insert_vortex_dipole(dipole_vector=[nem.xmax/3,0],
+            dipole_position=nem.rmid)
+        nem.evolve_nematic_no_flow(10)
+        nem.evolve_nematic(1)
+        Dnodes = nem.calc_vortex_nodes_nem()
 
-        # Set the tolerance for approximation
-        tolerance = 0.01
+        self.assertEqual(len(Dnodes), 2, "Dipole nodes not found")
 
-        # Check if all elements in bec.psi are approximately 1
-        S = np.sqrt(nem.B) / 2
-        condition = np.allclose(abs(nem.Q[0]), S, atol=tolerance)
-        self.assertTrue(condition, "Elements in nem.Q are not approximately 1")
+        # Create lists of the x and y positions of the nodes
+        xpos = [node['position'][0] for node in Dnodes]
+        ypos = [node['position'][1] for node in Dnodes]
+
+        # Sort the lists according to xpos
+        sorted_lists = sorted(zip(xpos, ypos))
+
+        # Unzip the sorted lists
+        xpos, ypos = zip(*sorted_lists)
+
+        # Testing the position of the first node
+        self.assertAlmostEqual(xpos[0], nem.xmax / 3, delta=1)
+        self.assertAlmostEqual(ypos[0], nem.ymax / 2, delta=1)
+
+        # Testing the position of the second node
+        self.assertAlmostEqual(xpos[1], 2 * nem.xmax / 3, delta=1)
+        self.assertAlmostEqual(ypos[1], nem.ymax / 2, delta=1)
+
+
 
 
 if __name__ == '__main__':
