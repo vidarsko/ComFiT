679be45e75eb89d4e6bc9c1e9a65bd8178daa217
jonasron <46788041+jonasron@users.noreply.github.com>
2023-12-28 10:52:09 +0900

    small changes

--- development/231031_jr_test_nematic_flow.py
+++ development/231031_jr_test_nematic_flow.py
@@ -8,13 +8,14 @@ nem = cf.NematicLiquidCrystal(2,xRes=64,yRes=64,dx=1,dy=1,dt=0.1)
 
 nem.conf_initial_condition_disordered(noise_strength=.1)
 #nem.evolve_nematic_no_flow(10)
-nem.conf_active_channel(20,7)
+nem.conf_active_channel(20,2)
 
 plt.figure()
 nem.plot_field(nem.alpha,cmap_symmetric=False,colormap="brg")
 #plt.plot([nem.xmid,nem.xmid],[nem.ymin,nem.ymax-1],ls=':',color ='w')
 #plt.plot([nem.xmid-10,nem.xmid-10],[nem.ymin,nem.ymax-1],ls=':',color ='w')
 #plt.plot([nem.xmid+10,nem.xmid+10],[nem.ymin,nem.ymax-1],ls=':',color ='w')
+plt.savefig("spatial_alpha.png",dpi=300)
 plt.show()
 
 #nem.evolve_nematic(400,"ETD4RK")
