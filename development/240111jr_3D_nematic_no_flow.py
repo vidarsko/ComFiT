aaf896f08f227348e3998a58a299c06447ab4493
jonasron <46788041+jonasron@users.noreply.github.com>
2024-01-17 15:28:53 +0900

    Update 240111jr_3D_nematic_no_flow.py

--- development/240111jr_3D_nematic_no_flow.py
+++ development/240111jr_3D_nematic_no_flow.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import comfit as cf
 
-nem = cf.NematicLiquidCrystal(3,xRes=31,yRes=31,zRes=31,dx=1,dy=1,dt=0.1,alpha=-2.0)
+nem = cf.NematicLiquidCrystal(3,xRes=31,yRes=31,zRes=31,dx=1,dy=1,dt=0.1,alpha=1.0)
 
 
 
