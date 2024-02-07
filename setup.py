b4de093735d8c92b97d4a5956ac0f4f14f645d46
Vidar Skogvoll <vidarsko@fys.uio.no>
2024-02-07 21:17:47 +0100

    Mayavi 4.7.0

--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@ setup(
                       'imageio',
                       'vtk==9.2.6',
                       'PyQt5==5.12.2',
-                      'mayavi==4.7.1'],
+                      'mayavi==4.7.0'],
 )
