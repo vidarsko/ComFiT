bdc7d87cc6aa1993d42c6eeac07b33f714bb0eb0
Vidar Skogvoll <vidarsko@fys.uio.no>
2024-02-23 16:36:14 +0100

    Added back scikit image

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     install_requires=['numpy',
                       'scipy',
                       'matplotlib',
+                      'scikit-image',
                       'moviepy',
                       'imageio'],
 )
