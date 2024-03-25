ae8c71eb9f6973391d0179dea10493e276719eef
jonasron <46788041+jonasron@users.noreply.github.com>
2024-03-25 16:42:14 +0900

    Update 240218jr_testing_disclination_density_#D_nematic.py

--- development/240218jr_testing_disclination_density_#D_nematic.py
+++ development/240218jr_testing_disclination_density_#D_nematic.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import comfit as cf
 
-nem =cf.NematicLiquidCrystal(3,xRes=32,yRes=32,zRes=32,dt=0.1,alpha=-1.2,C=1)
+nem =cf.NematicLiquidCrystal(3,xRes=32,yRes=32,zRes=32,dt=0.1,alpha=-1.2,C=0)
 
 #nem.conf_initial_condition_ordered(noise_strength=0.1)
 
@@ -16,8 +16,8 @@ S,n = nem.calc_order_and_director()
 #D = nem.calc_disclination_density_nematic()
 
 omega,Omega,T,trD = nem.calc_disclination_density_decoupled()
-#fig,ax =nem.plot_field(S,number_of_layers=2,vlim_symmetric=False)
-#nem.plot_vector_field(Omega,ax=ax)
+#fig,ax =nem.plot_field(omega,number_of_layers=2,vlim_symmetric=False)
+#nem.plot_vector_field(Omega)
 
 nodes = nem.calc_disclination_nodes_nem()
 #print(nodes)
