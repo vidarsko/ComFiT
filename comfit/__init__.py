9d44d860f1bdb4ca52d93d23c77a62fed158fecf
Vidar Skogvoll <vidarsko@fys.uio.no>
2025-02-04 11:40:18 +0100

    fixed import

--- comfit/__init__.py
+++ comfit/__init__.py
@@ -24,7 +24,7 @@ from .plot import plot_field_plotly, plot_field_matplotlib, \
     plot_vector_field_in_plane_matplotlib, plot_vector_field_in_plane_plotly, \
     plot_nodes_matplotlib, plot_nodes_plotly
 
-from .tool import tool_colormap_bluewhitered, tool_colormap_angle, tool_save_plot_matplotlib, tool_save_plot, \
+from .tool import tool_colormap_bluewhitered, tool_colormap_angle,  \
     tool_make_animation_movie, tool_make_animation_gif, \
     tool_export_rotating_plot, tool_zoom_plot, tool_colormap_sunburst, tool_multinom, \
     tool_set_plot_axis_properties_matplotlib, tool_set_plot_axis_properties_plotly, \
