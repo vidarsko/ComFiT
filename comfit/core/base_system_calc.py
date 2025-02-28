32d4e61768086fd342b9dab956c83871b7182cdd
Vidar Skogvoll <vidarsko@fys.uio.no>
2025-02-28 09:03:56 +0100

    RUn core tests

--- comfit/core/base_system_calc.py
+++ comfit/core/base_system_calc.py
@@ -741,7 +741,7 @@ class BaseSystemCalc:
 
         Args:
             omega_f: The value of omega_f  (numpy.ndarray)
-            tol: tolerance for when to expand the integrating factors that divide by omega (float,optional)
+            tol: tolerance for when to expand the integrating factors that divide by omega (float,optional).
          
         Returns:
             The list of integrating factors.
