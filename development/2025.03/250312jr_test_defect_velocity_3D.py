80394aa5c76709cb607fb9992890a0925cb39eb8
jonasron <46788041+jonasron@users.noreply.github.com>
2025-03-16 14:53:21 -0700

    Prøver å finne ut hvorfor det er en fortegnsfeil i hastigheten

--- development/2025.03/250312jr_test_defect_velocity_3D.py
+++ development/2025.03/250312jr_test_defect_velocity_3D.py
@@ -8,16 +8,16 @@ nem =cf.NematicLiquidCrystal(3,xRes=32,yRes=32,zRes=32,dx=1,dt=0.1,alpha=-0.8)
 
 nem.conf_initial_disclination_lines()
 
-nem.evolve_nematic_no_flow(90,method="ETD4RK")
+nem.evolve_nematic_no_flow(110,method="ETD4RK")
 Q_prev = np.copy(nem.Q)
-nem.evolve_nematic_no_flow(5)
+nem.evolve_nematic_no_flow(3)
 
-dt_Q = (nem.Q - Q_prev)/(5*nem.dt)
+dt_Q = (nem.Q - Q_prev)/(3*nem.dt)
 
-nodes = nem.calc_disclination_nodes_nem()
+nodes = nem.calc_disclination_nodes_nem(dt_Q)
 
 #print(nodes[3]['velocity'])
 
-fig,axes =nem.plot_nodes(nodes,plot_lib='matplotlib')
+fig,axes =nem.plot_nodes(nodes)
 
-plt.show()
+fig.show()
